062e9a2d1324826ad95c1c090d07447aaf764c90
marcelli <marcelli.faria@gmai.com>
2024-09-05 09:05:50 -0300

    verifica mensagem email e campo telefone

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -10,16 +10,33 @@ describe('Testes da Central de atendimento do cliente TAT', function () {
   })
 
   it ('preenche os campos obrigatórios e envia o formulário', () => {
+    const textoLongo = "Minha sincera gratidão pelo seu trabalho. Sua habilidade e empenho são um exemplo para todos nós. Sei que o caminho profissional nem sempre é fácil, mas você tem enfrentado desafios com coragem e perseverança, inspirando todos ao seu redor."
     cy.get('#firstName').type('Angelina')
     cy.get('#lastName').type('Jolie')
     cy.get('#email').type('angelina@mailinator.com')
     cy.get('#phone').type('21999999999')
-    cy.get('#open-text-area').type('estou satisfeito com o atendimento')
+    cy.get('#open-text-area').type(textoLongo, {delay:0})
     cy.get('button[type="submit"]').click()
     cy.get('.success').should('be.visible')
   })
 
+  it ('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
+  cy.get('#firstName').type('Brad')
+  cy.get('#lastName').type('Pitt')
+  cy.get('#email').type('mailinator.com')
+  cy.get('#phone').type('21999999999')
+  cy.get('#open-text-area').type('Não gostei do atendimento', {delay:0})
+  cy.get('button[type="submit"]').click()
+  cy.get('.error').should('be.visible')
+  })
 
+
+  it ('verifica se campo telefone somente aceita números', () =>{
+    cy.get('#phone').type('abcdefgh')
+      .should('have.value', '')
+
+
+  })
 })
 
 
