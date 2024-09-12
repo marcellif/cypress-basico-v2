478fb5ba636c601c70b8b32bedc65c6da619c027
marcelli <marcelli.faria@gmai.com>
2024-09-12 10:09:00 -0300

    request para testar http

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -10,7 +10,7 @@ describe('Testes da Central de atendimento do cliente TAT', function () {
     cy.title().should('eq','Central de Atendimento ao Cliente TAT');
   })
 
-  it.only ('preenche os campos obrigatórios e envia o formulário', () => {
+  it ('preenche os campos obrigatórios e envia o formulário', () => {
     cy.clock()
     const textoLongo = Cypress._.repeat(' Muito obrigada ', 40)
     cy.get('#firstName').type('Angelina')
@@ -171,6 +171,41 @@ describe('Testes da Central de atendimento do cliente TAT', function () {
     cy.contains('Talking About Testing')
       .should('be.visible')
   })
+
+
+  it('exibe e esconde as mensagens de sucesso e erro usando o .invoke', () => {
+    cy.get('.success')
+      .should('not.be.visible')
+      .invoke('show')
+      .should('be.visible')
+      .and('contain', 'Mensagem enviada com sucesso.')
+      .invoke('hide')
+      .should('not.be.visible')
+    cy.get('.error')
+      .should('not.be.visible')
+      .invoke('show')
+      .should('be.visible')
+      .and('contain', 'Valide os campos obrigatórios!')
+      .invoke('hide')
+      .should('not.be.visible')
+  })
+
+it ('preenche a area de texto usando o comando invoke', () => {
+  const longText = Cypress._.repeat(' 1234567890 ', 10)
+  cy.get('#open-text-area')
+    .invoke('val', longText)
+    .should('have.value', longText)
+})
+
+it ('faz uma requisição HTTP e verifica itens', () => {
+  cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+    .should(function(response){
+      const {status, statusText, body} = response;
+      expect(status).to.equal(200)
+      expect(statusText).to.equal('OK')
+      expect(body).to.include('CAC TAT')
+    })
+})
 
 })
 
